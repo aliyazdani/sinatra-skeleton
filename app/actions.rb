036a9b8ad8caae24d2531ded95bffb35b04cfe20
Ali Yazdani <aliyazdani82@gmail.com>
2015-04-19 18:51:15 -0700

    add to actions

--- app/actions.rb
+++ app/actions.rb
@@ -3,20 +3,27 @@ get '/' do
   erb :index
 end
 
+# Teams index
 get '/teams' do
   @teams = Team.all
   erb :teams
 end
 
+# Page that includes a form to create new team
 get '/teams/new' do
   erb :new_team
 end
 
+# Action to create a new team
 post '/teams/new' do
 end
 
+# show a particular team
 get '/teams/:id' do
+  @team = Team.find(params[:id])
+  erb :team
 end
 
+# create a player belonging to a team
 post '/teams/:id/players/new' do
 end
